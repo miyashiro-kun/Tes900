2023d68e54316ad2e2804928130a077ab25d0703
Miyashiro <32512576+miyashiro-kun@users.noreply.github.com>
2025-12-19 21:38:27 +0700

    Fix path for loading the first page

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -2,7 +2,7 @@ const indicator = document.querySelector(".indicator");
 const tabs = document.querySelectorAll(".tab");
 
 // Load first page by default
-loadPage("page/page1.html");
+loadPage("pages/page1.html");
 
 function changeTab(tab, page) {
     tabs.forEach(t => t.classList.remove("active"));
